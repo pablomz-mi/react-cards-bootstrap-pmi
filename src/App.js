ecc9f60c848d3d12dee4edb1864c2b500efebf39
pablomz-mi <pablomunozinigo@gmail.com>
2024-10-14 10:17:53 +0200

    proyecto github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Cards from './components/Cards';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Cards/>
+    </div>
+    
+  )
+}
+
+export default App
